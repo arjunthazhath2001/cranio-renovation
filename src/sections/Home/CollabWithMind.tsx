51c613f0dbb2777bd6654a90f6ee7f2925731821
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-31 00:18:50 +0530

    section2 ok

--- src/sections/Home/CollabWithMind.tsx
+++ src/sections/Home/CollabWithMind.tsx
@@ -12,10 +12,10 @@ function CollabWithMind() {
     return (
       <section className="bg-[linear-gradient(to_top,#ffe566,white,white,white)] flex flex-col items-center justify-center pb-20 px-8">
   
-        <div className="container px-4 ">
+        <div className="container px-4">
           <div className='flex gap-3 justify-center items-center'>
             <Image src={carnio} className='rounded-xl' alt="carn-logo"/>
-            <h1 className={`${inter.className} text-2xl text-shadow`}>X</h1>
+            <h1 className={`${inter.className} text-2xl text-shadow font-bold`}>X</h1>
             <Image src={mind} className='rounded-xl' alt="mat-logo"/>
           </div>
 
@@ -27,7 +27,7 @@ function CollabWithMind() {
             />
             
           </div>
-          <p className={`${inter.className} text-sm mt-4`}>In partnership with<span className='font-semibold'> the Johns Hopkins MIND</span> at Home program, we provide<span className='font-semibold'> free care navigation for dementia patients</span>, funded by Medicare’s GUIDE Model. Doctors maintain clinical control, while we offer 24/7 support between visits—improving patient outcomes and unlocking substantial<span className='font-semibold'> new revenue for each doctor.</span> </p>
+          <p className={`${inter.className} text-sm mt-4 tracking-tight leading-[30px]`}>In partnership with<span className='font-semibold'> the Johns Hopkins MIND</span> at Home program, we provide<span className='font-semibold'> free care navigation for dementia patients</span>, funded by Medicare’s GUIDE Model. Doctors maintain clinical control, while we offer 24/7 support between visits—improving patient outcomes and unlocking substantial<span className='font-semibold'> new revenue for each doctor.</span> </p>
         
         </div>
   
